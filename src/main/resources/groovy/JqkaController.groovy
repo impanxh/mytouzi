3da470c0881883d28c8e6f4978558854781c8a2a
panxinghua <impanxh@gmail.com>
2022-03-17 18:47:07 +0800

    缓存大盘评级结果 每30秒取一次 避免岩机可能

--- src/main/resources/groovy/JqkaController.groovy
+++ src/main/resources/groovy/JqkaController.groovy
@@ -10,11 +10,22 @@ import com.huangxifeng.gupiao.util.HtmlUtil
 @Controller
 @RequestMapping(value = "/10jqka")
 public class JqkaController {
+	
+	
+	def cache  = null;
+	def lastUpdate =  -1;
 	HtmlUtil  util = new HtmlUtil();
 	@RequestMapping(value = "/indexflash")
 	@ResponseBody
 	public def jqka(HttpServletRequest request) {
-		String url = "http://q.10jqka.com.cn/";
-		return util.work(url,"http://q.10jqka.com.cn/api.php?t=indexflash&");
+		if(cache == null  ||  (  ( System.currentTimeMillis() -lastUpdate) > 30000   )  ) {
+			println "fetch-from utl "
+			String url = "http://q.10jqka.com.cn/";
+			def r =  util.work(url,"http://q.10jqka.com.cn/api.php?t=indexflash&");
+			 cache = r;
+			 lastUpdate  = System.currentTimeMillis()
+			 return cache;
+		}
+		return cache;
 	}
 }
